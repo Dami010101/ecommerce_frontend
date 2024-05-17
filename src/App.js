7e3dbeffd1a5699daf885b633902840cdd94cef5
Dami010101 <143275747+Dami010101@users.noreply.github.com>
2024-05-17 21:37:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Browse_by_categories from './component/browse_by_category/Browse_by_categories';
+
+function App() {
+  return (
+    <div className="App">
+      <Browse_by_categories/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/browse_by_category/Browse_by_categories.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { TbDeviceMobile } from "react-icons/tb";
+import { BsSmartwatch } from "react-icons/bs";
+import { FaCameraRetro } from "react-icons/fa";
+import { FaHeadphones } from "react-icons/fa";
+import { MdComputer } from "react-icons/md";
+import { IoGameController } from "react-icons/io5";
+import { CiSpeaker } from "react-icons/ci";
+import { FaTabletScreenButton } from "react-icons/fa6";
+
+
+
+const Browse_by_categories = () => {
+  return (
+    <div>
+        <TbDeviceMobile />
+        <BsSmartwatch />
+        <FaCameraRetro />
+        <FaHeadphones />
+        <MdComputer />
+        <IoGameController />
+        <CiSpeaker />
+        <FaTabletScreenButton />
+
+    </div>
+  )
+}
+
+export default Browse_by_categories
