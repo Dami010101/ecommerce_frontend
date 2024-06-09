09735c4be6b08bb696af5f38b28542a0eac117fb
Dami010101 <143275747+Dami010101@users.noreply.github.com>
2024-06-09 16:49:38 +0100

    upi

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -64,7 +64,7 @@ const navStylez = 'text-black font-bold text-sm px-2 py-1 hover:bg-gray-100 hove
     const navContent = (
         <>
 
-        <div className={navStylez}><Link to='/'>Homej</Link></div>
+        <div className={navStylez}><Link to='/'>Homel</Link></div>
         <div className={navStylez}><Link to="/cart" className="btn btn-ghost btn-circle flex">
                             <MdShoppingCart className="h-6 w-6" />
                             {cartItemCount > 0 && (
